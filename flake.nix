156cae65f70f1977c293444acb45ffe2454da0a4
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2023-06-13 18:46:36 +0200

    Update Go to 1.19 (#37)

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A Nix wrapped development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/22.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05-small";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -18,7 +18,7 @@
           rec {
             buildInputs = with pkgs;
               [
-                go_1_18
+                go_1_19
                 gopls
                 goreleaser
                 gnumake
